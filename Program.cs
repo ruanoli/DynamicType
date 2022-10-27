f90d06efd1c12a45b6d707ef72487ba8adf79978
Ruan Oliveira <ruanoliveira.pro@gmail.com>
2022-10-27 19:12:22 -0300

    Dynamic type

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+dynamic exemple = 4;
+
+Console.WriteLine($"{exemple.GetType()} | {exemple}");
+
+exemple = "Ruan";
+Console.WriteLine($"{exemple.GetType()} | {exemple}");
+
+exemple = 25.40M;
+Console.WriteLine($"{exemple.GetType()} | {exemple}");
+
+exemple = true;
+Console.WriteLine($"{exemple.GetType()} | {exemple}");
